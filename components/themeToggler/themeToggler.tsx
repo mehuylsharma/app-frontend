7a2c9cd9c5f7b835087cc2dc2445368d8eb97d4e
irfan shaik <irfanshaik11@gmail.com>
2022-06-15 08:20:45 -0400

    change color scheme

--- components/themeToggler/themeToggler.tsx
+++ components/themeToggler/themeToggler.tsx
@@ -8,7 +8,7 @@ const ThemeToggler: FC = () => {
 
   return (
     <ThemeTogglerStyle onClick={toggleTheme}>
-      {themeName === 'dark' ? <Light /> : <Dark />}
+      {themeName === 'light' ? <Dark /> : <Light />}
     </ThemeTogglerStyle>
   );
 };
